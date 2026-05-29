1ef61bbeb889576e0bfcdaf017cde0945b275c33
Odeyale Gbenga <gbengha2016@gmail.com>
2026-05-29 17:05:39 +0100

    feat: community reviews, likes on feed posts, public live data

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from .models import ChatMessage
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import action
 
 
 class ChatMessagesView(APIView):
