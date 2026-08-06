c08405816e34f898be0e1528f1ca36ad2379c10e
Odeyale Gbenga <gbengha2016@gmail.com>
2026-08-06 18:24:30 -0400

    Add ForexForecast import and ensure model is migrated

--- apps/forex_ea/forex_api.py
+++ apps/forex_ea/forex_api.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import ForexCache
+from .models import ForexCache, ForexForecast
 
 class ForexDailyView(APIView):
     authentication_classes = []
